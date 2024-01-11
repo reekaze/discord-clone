a45286173931078e14a8fb4cdd0c80cb81bb007e
reekaze <tan.albert53@gmail.com>
2024-01-12 02:36:56 +0700

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AuthLayout = ({ children }: Props) => {
+  return (
+    <div className="h-full flex justify-center items-center">{children}</div>
+  );
+};
+
+export default AuthLayout;
